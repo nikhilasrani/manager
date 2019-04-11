d0a23ad068bba2cf5a1ad9b5051fa2f0e123257c
Nikhil Asrani <asrani.nikhil@gmail.com>
2019-04-11 06:31:51 +0530

    completed login functionality

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
+import { View, Text } from "react-native";
 import { connect } from "react-redux";
-import { Card, CardSection, Input, Button } from "./common";
+import { Card, CardSection, Input, Button, Spinner } from "./common";
 import { emailChanged, passwordChanged, loginUser } from "../actions";
 
 class LoginForm extends Component {
@@ -12,8 +13,24 @@ class LoginForm extends Component {
   }
   onButtonPress() {
     const { email, password } = this.props;
-
     this.props.loginUser({ email, password });
+  }
+
+  renderError() {
+    if (this.props.error) {
+      return (
+        <View style={{ backgroundColor: "white" }}>
+          <Text style={styles.errorTextStyle}>{this.props.error}</Text>
+        </View>
+      );
+    }
+  }
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size="large" />;
+    } else {
+      return <Button onPress={this.onButtonPress.bind(this)}>Login</Button>;
+    }
   }
 
   render() {
@@ -36,18 +53,26 @@ class LoginForm extends Component {
             value={this.props.password}
           />
         </CardSection>
-        <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>Login</Button>
-        </CardSection>
+        {this.renderError()}
+        <CardSection>{this.renderButton()}</CardSection>
       </Card>
     );
   }
 }
 
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: "center",
+    color: "red"
+  }
+};
 const mapStateToProps = state => {
   return {
     email: state.auth.email,
-    password: state.auth.password
+    password: state.auth.password,
+    error: state.auth.error,
+    loading: state.auth.loading
   };
 };
 
