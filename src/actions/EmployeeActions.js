7973a78d67c78e295918fbe7c4b37b6aec32fa3b
Nikhil Asrani <asrani.nikhil@gmail.com>
2019-04-13 16:50:41 +0530

    Added Employee Creation and Fetching functionality

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,7 +1,40 @@
-import { EMPLOYEE_UPDATE } from "./types";
+import firebase from "firebase";
+import {
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
+} from "./types";
+import { Actions } from "react-native-router-flux";
+
 export const employeeUpdate = ({ prop, value }) => {
   return {
     type: EMPLOYEE_UPDATE,
     payload: { prop, value }
+  };
+};
+
+export const employeeCreate = ({ name, phone, shift }) => {
+  const { currentUser } = firebase.auth();
+  return dispatch => {
+    firebase
+      .database()
+      .ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      .then(() => {
+        dispatch({ type: EMPLOYEE_CREATE });
+        Actions.pop({ type: "reset" });
+      });
+  };
+};
+
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth();
+  return dispatch => {
+    firebase
+      .database()
+      .ref(`/users/${currentUser.uid}/employees`)
+      .on("value", snapshot => {
+        dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+      });
   };
 };
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { connect } from "react-redux";
+import { ListView, View, Text } from "react-native";
+import { employeesFetch } from "../actions";
 
 class EmployeeList extends Component {
+  componentWillMount() {
+    this.props.employeesFetch();
+
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.dataSource = ds.cloneWithRows(this.props.employees);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    //nextProps are the next set of props that this component will be rendered with
+    //this.props is still the old set of props
+  }
+
   render() {
     return (
       <View>
@@ -17,4 +33,7 @@ class EmployeeList extends Component {
   }
 }
 
-export default EmployeeList;
+export default connect(
+  null,
+  { employeesFetch }
+)(EmployeeList);
