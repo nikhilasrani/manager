a1c4d32d64565129b98f602d9ad5bafc63d8e11a
Nikhil Asrani <asrani.nikhil@gmail.com>
2019-04-14 15:54:48 +0530

    Added FlatList and removed depracted ListView

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,42 +1,37 @@
+import _ from "lodash";
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { ListView, View, Text } from "react-native";
+import { Text, View, FlatList } from "react-native";
 import { employeesFetch } from "../actions";
-
+import ListItem from "./ListItem";
 class EmployeeList extends Component {
-  componentWillMount() {
+  keyExtractor = item => item.name;
+  componentDidMount() {
     this.props.employeesFetch();
-    this.createDataSource(this.props);
   }
 
-  componentWillReceiveProps(nextProps) {
-    //nextProps are the next set of props that this component will be rendered with
-    //this.props is still the old set of props
-    this.createDataSource(nextProps);
+  renderRow({ item }) {
+    return <ListItem employee={item} {...this.props} />;
   }
-
-  createDataSource({ employees }) {
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
-    this.dataSource = ds.cloneWithRows(employees);
-  }
-
   render() {
     return (
-      <View>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-      </View>
+      <FlatList
+        data={this.props.employees}
+        keyExtractor={this.keyExtractor}
+        renderItem={this.renderRow.bind(this)}
+      />
     );
   }
 }
+const mapStateToProps = ({ employees }) => {
+  return {
+    employees: _.map(employees, (value, uid) => {
+      return { ...value, uid };
+    })
+  };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   { employeesFetch }
 )(EmployeeList);
